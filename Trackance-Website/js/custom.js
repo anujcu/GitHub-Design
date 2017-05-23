2b35420ae9a915662b95490d1cfb2ab016652735
anujcu <anujcu@gmail.com>
2017-05-23 19:03:33 +0530

    Final Pages

--- Trackance-Website/js/custom.js
+++ Trackance-Website/js/custom.js
@@ -1,4 +1,11 @@
-       /*JQuery Box Slider Responsive*/
+       /*scrolling js*/
+   new WOW().init();
+      //scrolling function end
+   /* smooth scrolling for scroll to top */
+$('.scrolltop').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+});
+/*JQuery Box Slider Responsive*/
        (function () {
            loader = new SVGLoader(document.getElementById('loader'), {
                speedIn: 400,
@@ -18,20 +25,9 @@
 
        })();
        /*Wow Js Function */
-       new WOW().init();
        /* Google Nexus Menu Function*/
        new gnMenu(document.getElementById('gn-menu'));
-       //          /header:hover*/
-       $(function () {
-           $('.dropdown').hover(
-               function () {
-                   $(this).addClass('open');
-               },
-               function () {
-                   $(this).removeClass('open');
-               }
-           );
-       });
+
        /*HeaderCSS*/
        jQuery(window).scroll(function () {
            if (jQuery(".navbar-default").offset().top > 300) {
@@ -49,42 +45,15 @@
 
            }
        });
-       /*scrolling js*/
 
-       // Select all links with hashes
-       $('a[href*="#"]')
-           // Remove links that don't actually link to anything
-           .not('[href="#"]')
-           .not('[href="#0"]')
-           .click(function (event) {
-               // On-page links
-               if (
-                   location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                   location.hostname == this.hostname
-               ) {
-                   // Figure out element to scroll to
-                   var target = $(this.hash);
-                   target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                   // Does a scroll target exist?
-                   if (target.length) {
-                       // Only prevent default if animation is actually gonna happen
-                       event.preventDefault();
-                       $('html, body').animate({
-                           scrollTop: target.offset().top - 65
-                       }, 1000, function () {
-                           // Callback after animation
-                           // var targetOffset = $target.offset().top - 70;
-                           // Must change focus!
-                           var $target = $(target);
-                           $target.focus();
-                           if ($target.is(":focus")) { // Checking if the target was focused
-
-                               return false;
-                           } else {
-                               $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                               $target.focus(); // Set focus again
-                           };
-                       });
-                   }
+       //          /header:hover*/
+       $(function () {
+           $('.dropdown').hover(
+               function () {
+                   $(this).addClass('open');
+               },
+               function () {
+                   $(this).removeClass('open');
                }
-           });
+           );
+       });
